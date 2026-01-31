f05f715969591424fbfc2e034e8706a4dc78d9b6
Ketaki Kulkarni <140495569+Kool-K@users.noreply.github.com>
2026-01-31 17:03:59 +0530

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Scene from "./components/Scene.jsx";
+
+export default function App() {
+  return (
+    <div style={{ padding: "24px" }}>
+      <h1>The Arboretum</h1>
+      <Scene />
+    </div>
+  );
+}
